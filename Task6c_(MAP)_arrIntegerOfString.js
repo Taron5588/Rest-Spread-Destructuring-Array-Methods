8ab7afce9fb40ec3518d11ea42e0ec3cb3528b47
TaronKarapetyan <taronvkarapetyan@gmail.com>
2023-03-09 01:39:30 +0400

    fix: added logical operator in ternary operator

--- Task6c_(MAP)_arrIntegerOfString.js
+++ Task6c_(MAP)_arrIntegerOfString.js
@@ -5,8 +5,8 @@
 
 // console.log(parseInt(["1", "2", "px", "10"]))
 function parseInt(array) {
-    let newArr = array.map((value) => isFinite(value) ? (+value) : null)
+    let newArr = array.map((value) => isFinite(value) && value ? (+value) : null)
     return newArr
 }
 
-console.log(parseInt(["1", "2", "px", "10"]))
+console.log(parseInt(["1", "2", false, "10"]))  //In "", false cases returnes null.
