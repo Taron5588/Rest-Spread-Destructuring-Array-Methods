a441cff51e658c9de5e1c91d6f94d0b95a7d6562
TaronKarapetyan <taronvkarapetyan@gmail.com>
2023-03-10 09:35:55 +0400

    fix: add reduce method

--- Task8b_(REDUCE)AgeOfUsers.js
+++ Task8b_(REDUCE)AgeOfUsers.js
@@ -10,11 +10,13 @@ const users = [
     age: 54,
     },
     ];
+
 function getAverageAge(users) {
     const [{age : first}, {age : second}] = users
-    return (first + second)/2
-    // const getAverageAge = users.filter((user) => user.age)
-    // console.log(getAverageAge)
-    //const sumAverageAge = users.reduce((acc, cur) => (acc + cur)/2  )
+    return (first + second)/users.length
+    //orrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+    // sum = users.reduce((acc, cur) => (acc.age + cur.age)/users.length)
+    // return sum
 }
+
     console.log(getAverageAge(users)); // 55
